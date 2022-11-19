5ccd2dd4002de75f1ba909e1d8953afb1afe8ca9
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2022-11-19 14:02:24 -0800

    add 404 page route

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ const app = express()
 app.get('/', (req, res)=> {
     res.send("Hello World!")
 })
+
+// KEEP THIS AT THE BOTTOM! (catch-all route)
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
 app.listen(process.env.PORT)
