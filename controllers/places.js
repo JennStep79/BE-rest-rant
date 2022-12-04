dc0866277ae2887011c47dd70d8869a6a9ddf6b7
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2022-12-03 16:15:35 -0800

    delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -38,4 +38,22 @@ router.post('/', (req, res) => {
     }
   })
 
+  router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+            res.render('error404')
+        }
+        else {
+            places.splice(id, 1)
+            res.redirect('/places')
+        }
+  })
+
+  router.get('/:id/edit', (req, res) => {
+    res.send('GET /places/:id/edit stub')
+  })
+
 module.exports = router
